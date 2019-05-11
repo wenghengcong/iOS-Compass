503d9265a8316665cf91cd515b0ee4f4286cce81
wenghengcong <wenghengcong@icloud.com>
2019-05-11 17:12:27 +0800

    Updates

---
+++ precache-manifest.ba382a0d273f5ef61ddbdf06fb345d13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69e46f9d7cd0eac51af25a1e59d838d",
+    "url": "/iOS-Compass/index.html"
+  },
+  {
+    "revision": "6c91ab197a6e82512bb2",
+    "url": "/iOS-Compass/static/css/2.25d8e7cb.chunk.css"
+  },
+  {
+    "revision": "38704629a7a6567ea80b",
+    "url": "/iOS-Compass/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "6c91ab197a6e82512bb2",
+    "url": "/iOS-Compass/static/js/2.9fe6d88a.chunk.js"
+  },
+  {
+    "revision": "38704629a7a6567ea80b",
+    "url": "/iOS-Compass/static/js/main.ba3b61b2.chunk.js"
+  },
+  {
+    "revision": "34848f53cc5ab43c51ec",
+    "url": "/iOS-Compass/static/js/runtime~main.67f735d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iOS-Compass/static/media/logo.5d5d9eef.svg"
+  }
+]);
