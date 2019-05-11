4ddf90e2f31deed8a395258b87cc2ebf5f4dd73f
wenghengcong <wenghengcong@icloud.com>
2019-05-11 17:19:19 +0800

    init pro

---
+++ compass/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import {
+  Button
+} from 'antd';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        哈哈
+        <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
